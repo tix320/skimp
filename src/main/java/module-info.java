ecff74d52e6f3e0ddb6a44f11a98e73779f7cbaf
Tigran Sargsyan <tixtix320@gmail.com>
2021-02-09 16:18:38 +0400

    Remove empty exports.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,8 @@
 module com.github.tix.skimp {
-	exports com.github.tix320.skimp.api.check;
 	exports com.github.tix320.skimp.api.collection;
 	exports com.github.tix320.skimp.api.exception;
 	exports com.github.tix320.skimp.api.function;
 	exports com.github.tix320.skimp.api.generator;
 	exports com.github.tix320.skimp.api.object;
-	exports com.github.tix320.skimp.api.reflection;
 	exports com.github.tix320.skimp.api.thread;
 }
