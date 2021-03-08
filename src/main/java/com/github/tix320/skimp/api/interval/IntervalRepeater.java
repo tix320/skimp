4f903838dc1783aa6874e042dc5460fe248e9798
Tigran Sargsyan <tixtix320@gmail.com>
2021-03-08 22:28:55 +0400

    Improve IntervalRepeater.

--- src/main/java/com/github/tix320/skimp/api/interval/IntervalRepeater.java
+++ src/main/java/com/github/tix320/skimp/api/interval/IntervalRepeater.java
@@ -3,52 +3,62 @@ package com.github.tix320.skimp.api.interval;
 
 import java.time.Duration;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 import com.github.tix320.skimp.api.exception.ExceptionUtils;
+import com.github.tix320.skimp.api.exception.ThreadInterruptedException;
+import com.github.tix320.skimp.api.function.CheckedRunnable;
+import com.github.tix320.skimp.api.function.CheckedSupplier;
 import com.github.tix320.skimp.api.object.None;
 
 public final class IntervalRepeater<T> {
 
-	private final Supplier<T> action;
-
 	private final Interval interval;
 
-	private IntervalRepeater(Supplier<T> action, Interval interval) {
-		this.action = action;
+	private final CheckedSupplier<T> action;
+
+	private IntervalRepeater(Interval interval, CheckedSupplier<T> action) {
 		this.interval = interval;
+		this.action = action;
 	}
 
-	public T doNext() throws InterruptedException {
+	public T doNext() throws ThreadInterruptedException, ActionFailedException {
 		Duration duration = interval.next();
-		Thread.sleep(duration.toMillis());
-		return action.get();
+		try {
+			Thread.sleep(duration.toMillis());
+		} catch (InterruptedException e) {
+			throw new ThreadInterruptedException();
+		}
+		try {
+			return action.get();
+		} catch (Exception e) {
+			throw new ActionFailedException(e);
+		}
 	}
 
-	public Optional<T> doUntilSuccess(int maxCount) throws InterruptedException {
+	public Optional<T> doUntilSuccess(int maxCount) throws ThreadInterruptedException {
 		int remaining = maxCount;
 		while (remaining != 0) {
 			try {
 				return Optional.ofNullable(doNext());
-			} catch (InterruptedException e) {
-				throw e;
-			} catch (Throwable e) {
-				ExceptionUtils.applyToUncaughtExceptionHandler(e);
+			} catch (ThreadInterruptedException e) {
+				throw new ThreadInterruptedException();
+			} catch (ActionFailedException e) {
 				remaining--;
+				ExceptionUtils.applyToUncaughtExceptionHandler(e);
 			}
 		}
 
 		return Optional.empty();
 	}
 
-	public static <T> IntervalRepeater<T> of(Supplier<T> runnable, Interval interval) {
-		return new IntervalRepeater<>(runnable, interval);
+	public static <T> IntervalRepeater<T> of(Interval interval, CheckedSupplier<T> runnable) {
+		return new IntervalRepeater<>(interval, runnable);
 	}
 
-	public static IntervalRepeater<None> of(Runnable runnable, Interval interval) {
-		return of(() -> {
+	public static IntervalRepeater<None> of(Interval interval, CheckedRunnable runnable) {
+		return of(interval, () -> {
 			runnable.run();
 			return None.SELF;
-		}, interval);
+		});
 	}
 }
