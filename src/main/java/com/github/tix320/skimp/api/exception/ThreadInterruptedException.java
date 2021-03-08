c92bbd1b459b2b4caac01d4a1ed8ffc052c5c176
Tigran Sargsyan <tixtix320@gmail.com>
2021-03-08 22:29:25 +0400

    Add unchecked InterruptedException version.

---
+++ src/main/java/com/github/tix320/skimp/api/exception/ThreadInterruptedException.java
@@ -0,0 +1,9 @@
+package com.github.tix320.skimp.api.exception;
+
+/**
+ * Unchecked version of {@link InterruptedException}
+ *
+ * @author : Tigran Sargsyan
+ * @since : 08.03.2021
+ **/
+public class ThreadInterruptedException extends RuntimeException {}
