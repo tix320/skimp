641681f5aad3219e104c0dec5c96bb6e0caac114
Tigran Sargsyan <tixtix320@gmail.com>
2021-03-08 22:28:55 +0400

    Improve IntervalRepeater.

---
+++ src/main/java/com/github/tix320/skimp/api/interval/ActionFailedException.java
@@ -0,0 +1,12 @@
+package com.github.tix320.skimp.api.interval;
+
+/**
+ * @author : Tigran Sargsyan
+ * @since : 08.03.2021
+ **/
+public class ActionFailedException extends RuntimeException {
+
+	public ActionFailedException(Throwable cause) {
+		super("Interval action fails.", cause);
+	}
+}
