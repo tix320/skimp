b60a9c1bc8d7cf2b3bd8aff185f45606a5e94b33
Tigran Sargsyan <tixtix320@gmail.com>
2021-11-29 10:14:16 -0800

    Redesign BiMap API.

---
+++ src/main/java/com/github/tix320/skimp/api/collection/map/mutable/MutableSortedBiMap.java
@@ -0,0 +1,5 @@
+package com.github.tix320.skimp.api.collection.map.mutable;
+
+import com.github.tix320.skimp.api.collection.map.SortedBiMap;
+
+public interface MutableSortedBiMap<F, S> extends SortedBiMap<F, S>, MutableBiMap<F, S> {}
